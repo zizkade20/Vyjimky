3086ff8467d8a5905c921e7618b3e8e545dfea37
zizka <zizkade20@sps-prosek.cz>
2023-03-12 21:54:47 +0100

    ahoj

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -13,6 +13,11 @@ namespace Vyjimky
     {
         static void Main()
         {
+            /*
+             * TRY-CATCH
+            
+            Console.WriteLine("Vyberte index záznamu");
+
             bool a = true;
             while (a)
             {
@@ -25,7 +30,6 @@ namespace Vyjimky
                         Console.WriteLine(ind + ") " + i);
                         ind++;
                     }
-                    Console.WriteLine("Vyberte index záznamu");
                     int entry = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine(myNumbers[entry]);
                     a = false;
@@ -37,6 +41,49 @@ namespace Vyjimky
                     a = true;
                 }
             }
+            */
+
+            // VLASTNI VYJIMKA
+            Console.WriteLine("Vyberte index záznamu");
+
+            bool a = true;
+            while (a)
+            {
+                try
+                {
+                    int[] myNumbers = { 6, 62, 331 };
+                    int ind = 1;
+
+
+
+                    foreach (int i in myNumbers)
+                    {
+                        Console.WriteLine(ind + ") " + i);
+                        ind++;
+                    }
+                    Console.Write("->");
+                    int entry = Convert.ToInt32(Console.ReadLine());
+                    if (entry >= ind) { throw new CustomException(entry + " je mimo array (" + myNumbers.Length + ")"); }
+
+                    Console.WriteLine(myNumbers[entry-1]);
+
+                }
+                catch (CustomException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch(FormatException ex) { Console.WriteLine(ex.Message); }
+
+            }
+            
+
+        }
+    }
+    class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+
         }
     }
 }
